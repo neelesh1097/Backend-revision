ff9803c5eb0098120c65b3284fe8d0140a2952b5
Neelesh_1097 <126332399+neelesh1097@users.noreply.github.com>
2025-11-21 00:06:59 +0530

    Add user and search routes to the app

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ app.use(express.json());
 app.get('/' , (req,res) => {
     res.send("the work of art")
 })
+app.get("/user/:username" , (req,res) => {
+    const username =req.params.username;
+    res.send(`welcome ${username}`)
+})
 
+app.get("/search" , (req,res) => {
+    const keyword =req.query.keyword;
+    res.send(`welcome ${keyword}`)
+})
 app.listen(5000);
